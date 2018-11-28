a519a0494ad2ea8ad081c2236a2699100fad3349
tecepe <tecepe@tecepe.eng.br>
2018-11-27 23:11:28 -0500

    adds a script to create a container with a homefs shared folder between container and host.

---
+++ run-container.sh
@@ -0,0 +1,12 @@
+## must be executed from test-docker folder
+
+mkdir -p homefs
+
+docker run -it --rm --name=dovecot-container \
+    -p 127.0.0.1:110:110/tcp \
+    -p 127.0.0.1:143:143/tcp \
+    -p 127.0.0.1:993:993/tcp \
+    -p 127.0.0.1:994:994/tcp \
+    -v $PWD/homefs:/home \
+    dovecot-img
+
